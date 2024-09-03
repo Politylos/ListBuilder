b5a6e8f18a17f178fc27ff152cdafd52dabe125f
Sophia Politylo <politylosophia@gmail.com>
2024-09-03 17:04:46 +1000

    added functions to load unit data

---
+++ app/functions/ListFunctions.tsx
@@ -0,0 +1,8 @@
+import * as FileSystem from 'expo-file-system';
+import React, { useState, useEffect } from 'react';
+var DOMParser = require('xmldom').DOMParser;
+import { jsonDict } from "@/app/functions/Structs";
+
+export async function CreateFactionList(catfile : string, name : string, Listsize : number){
+    return
+}
--- app/functions/LoadData.tsx
+++ app/functions/LoadData.tsx
@@ -1,10 +1,30 @@
 import * as FileSystem from 'expo-file-system';
 import React, { useState, useEffect } from 'react';
 var DOMParser = require('xmldom').DOMParser;
+import { jsonDict } from "@/app/functions/Structs";
 
-interface jsonDict<TValue> {
-  [id: string]: TValue;
+
+const IDict : jsonDict<string> = {
+  "Movement": "e703-ecb6-5ce7-aec1",
+  "Toughness": "d29d-cf75-fc2d-34a4",
+  "Save": "450-a17e-9d5e-29da",
+  "Wounds": "750a-a2ec-90d3-21fe",
+  "Leadership":"58d2-b879-49c7-43bc",
+  "OC":"bef7-942a-1a23-59f8",
+  "Range":"914c-b413-91e3-a132",
+  "Attacks":"2337-daa1-6682-b110",
+  "WS":"95d1-95f-45b4-11d6",
+  "Strength":"ab33-d393-96ce-ccba",
+  "AP":"41a0-1301-112a-e2f2",
+  "Damage":"3254-9fe6-d824-513e",
+  "Keywords":"893f-9000-ccf7-648e",
+  "Cost":"51b2-306e-1021-d207",
+  "Description":"9b8f-694b-e5e-b573"
+
 }
+
+
+
 
 function loadfaction(){
     var data = ""//require(file);
@@ -152,46 +172,80 @@ export async  function PraseCat(file : string){
 }
 export async function FindTag(element : any,tag : string){
   if (element.hasOwnProperty("nodeName")){
-    console.log(tag);
-    console.log(element.nodeName);
     if (element.nodeName == tag){
       return element;
     }
   }
   if (element.hasOwnProperty("nextSibling")){
-    console.log("sib");
     if (element.nextSibling != null){
       if (Array.isArray(element.nextSibling)){
         for (const sib_ele of element.nextSibling){
-          return FindTag(sib_ele,tag);
+          return await FindTag(sib_ele,tag);
         }
       } else if ((element.nextSibling.hasOwnProperty("data") |(element.nextSibling.hasOwnProperty("localName"))| (element.nextSibling.hasOwnProperty("firstChild")))){
-        return FindTag(element.nextSibling,tag);
+        return await FindTag(element.nextSibling,tag);
       } else if (element.nextSibling.hasOwnProperty("1")){
-        return FindTag(element.nextSibling[0],tag);
+        return await FindTag(element.nextSibling[0],tag);
       }
       
     }
   }
   if (element.hasOwnProperty("childNodes")){
-    console.log("choild");
     if (element.childNodes != null){
       if (Array.isArray(element.childNodes)){
-        for (const child_ele of element.childNodes){
-          return FindTag(child_ele,tag);
-        }
+          return await FindTag(element.childNodes[0],tag);
       } else if ((element.childNodes.hasOwnProperty("data") |(element.childNodes.hasOwnProperty("localName"))| (element.childNodes.hasOwnProperty("firstChild")))){
-        return FindTag(element.childNodes,tag);
+        return await FindTag(element.childNodes,tag);
       } else if  (element.childNodes.hasOwnProperty("1")){
-        return FindTag(element.childNodes[0],tag);
+        return await FindTag(element.childNodes[0],tag);
       }
     }
   }
   
 }
-export async function GetUnits(file: string){
-  var data = await PraseCat(file);
-  var units_xml = await FindTag(data, "categoryEntries");
+
+export async function MoveOverUnit(data: any, store: any){
+  if (data.hasOwnProperty("attributes")){
+    for (const att of Object.keys(data.attributes)){
+      if ((data.attributes[att].hasOwnProperty("name")) & (data.attributes[att].hasOwnProperty("nodeValue"))){
+        store[data.attributes[att]["name"]]=data.attributes[att]["nodeValue"];
+      }
+    }
+  }
+  if (data.hasOwnProperty("childNodes")){
+    for (const child_ele of Object.keys(data.childNodes)){
+      var elementData : jsonDict<any> = {}
+      //store[] = MoveOverUnit(data,store);
+    }
+  }
+  return store;
+}
+
+export async function GetUnitData(name: string, data: any){
+  
+  var units_xml = await FindTag(data, "sharedSelectionEntries");
+  for (const child_ele of Object.keys(units_xml.childNodes)){
+    if (units_xml.childNodes[child_ele].hasOwnProperty("attributes")){
+      var elementData : jsonDict<any> = {}
+      for (const att of Object.keys(units_xml.childNodes[child_ele].attributes)){
+        if ((units_xml.childNodes[child_ele].attributes[att].hasOwnProperty("name")) & (units_xml.childNodes[child_ele].attributes[att].hasOwnProperty("nodeValue"))){
+          if(units_xml.childNodes[child_ele].attributes[att]["name"] == "name"){
+            if (units_xml.childNodes[child_ele].attributes[att]["nodeValue"] == name){
+              elementData = await MoveOverUnit(units_xml.childNodes[child_ele],elementData)
+              return units_xml.childNodes[child_ele]
+            } else{
+              break;
+            }
+          } 
+        }
+      }
+    }
+  }
+  return null;
+}
+export async function GetUnits(data: any){
+  
+  var units_xml = await FindTag(data, "entryLinks");//await FindTag(data, "categoryEntries");
   var units: any[] = [];
   for (const child_ele of Object.keys(units_xml.childNodes)){
     if (units_xml.childNodes[child_ele].hasOwnProperty("attributes")){
@@ -201,23 +255,298 @@ export async function GetUnits(file: string){
           elementData[units_xml.childNodes[child_ele].attributes[att]["name"]]=units_xml.childNodes[child_ele].attributes[att]["nodeValue"];
         }
         
-        //console.log(units_xml.childNodes[child_ele].attributes[att]);
       }
       if (Object.keys(elementData).length > 0){
         units.push(elementData);
       }
     }
   }
-  console.log(units);
+  return units;
 }
+
+
+
+export async function StepOverProf(data: any, tag : string,jsonData : any,keyStack : any){
+  if(tag == "#text"){
+    
+    if((data.data.trim() == "\n") || (data.data.trim() == "")){
+      if (data.hasOwnProperty("nextSibling")){
+        if (data.nextSibling != null){
+            jsonData = await StepOverProf(data.nextSibling,data.nextSibling.nodeName,jsonData,keyStack);
+        }
+      }
+    } else {
+      jsonData["value"] = data.data;
+    }
+  }
+  else if (data.nodeName == tag){
+    
+    var subjson : jsonDict<any> = {} ; 
+    var ID = 0;
+    var IDexists = 0
+    if (Object.keys(data.attributes).length > 1){
+      for (const att of Object.keys(data.attributes)){
+        if ((data.attributes[att].hasOwnProperty("name")) && (data.attributes[att].hasOwnProperty("nodeValue"))){
+          if (data.attributes[att].name== "id"){
+            ID = data.attributes[att].nodeValue;
+            IDexists=1;
+          }
+          if ((data.attributes[att].name== "typeId") && (!IDexists)){
+            ID =data.attributes[att].nodeValue
+          }
+          subjson[data.attributes[att].name] = data.attributes[att].nodeValue;
+        }
+
+        
+      }
+      jsonData[ID] = subjson;
+    }
+    
+    if (data.hasOwnProperty("firstChild")){
+      
+      if (data.firstChild != null){  
+           
+
+          if (Object.keys(jsonData).length > 0 ){
+
+            var childreturn = await StepOverProf(data.firstChild,data.firstChild.nodeName,{},keyStack);
+
+            if ((Object.keys(childreturn).length < 3) && (childreturn.hasOwnProperty("value"))){
+              jsonData[ID]["value"] = childreturn["value"]
+            }
+            else{
+              if (ID != 0){
+                for (const att of Object.keys(childreturn)){
+                  jsonData[ID][att] = childreturn[att];
+                }
+              }else{
+                for (const att of Object.keys(childreturn)){
+                  jsonData[att] = childreturn[att];
+                }
+              }
+            }
+          }else {
+            
+            if(data.nodeName != "#text"){
+               jsonData = await StepOverProf(data.firstChild,data.firstChild.nodeName,jsonData,keyStack);
+            } else {
+              jsonData= await StepOverProf(data.firstChild,data.firstChild.nodeName,jsonData,keyStack);
+            }
+          }
+      }
+    }
+    
+  }
+  if(tag != "#text"){
+    if (data.hasOwnProperty("nextSibling")){
+      if (data.nextSibling != null){
+          
+          if (data.nextSibling.nodeName != "#text"){
+            jsonData = await StepOverProf(data.nextSibling,data.nextSibling.nodeName,jsonData,keyStack);//tag,jsonData,keyStack);
+          } else{
+            jsonData = await StepOverProf(data.nextSibling,tag,jsonData,keyStack);
+          }
+      }
+    }
+  }
+  return jsonData
+}
+
+export async function GetUnitcat(data : any){
+  return await GetUnitSection(data, "categoryLinks")
+}
+
+export async function GetUnitMods(data : any){
+  return await GetUnitSection(data, "modifiers")
+}
+
+export async function GetUnitsectionEntries(data : any){
+  return await GetUnitSection(data, "selectionEntries")
+}
+
+export async function GetUnitsectionGroups(data : any){
+  return await GetUnitSection(data, "selectionEntryGroups")
+}
+
+export async function GetUnitcosts(data : any){
+  return await GetUnitSection(data, "costs")
+}
+export async function GetUnitLinks(data : any){
+  return await GetUnitSection(data, "infoLinks")
+}
+
+export async function GetUnitSection(data : any,tagname : string){
+  var xml_profile = null;
+  if (data.childNodes != null){
+    if (1){
+      for (const child of Object.keys(data.childNodes)){
+        if (data.childNodes[child].nodeName == tagname){
+          xml_profile = data.childNodes[child];
+          var jsonprof : jsonDict<any> = {}
+          if (tagname == "selectionEntries"){
+            return StepOverProf(data.childNodes[child].firstChild,"selectionEntry",jsonprof,"");
+          } else {
+            return StepOverProf(data.childNodes[child].firstChild,tagname.substring(0, tagname.length - 1),jsonprof,"");
+          }
+        }
+    }  
+  }
+ 
+ }
+ return null; 
+}
+
+export async function GetUnitProf(data : any){
+  return await GetUnitSection(data, "profiles")
+}
+
+export async function GetAllUnitCosts(catFile : string){
+  var Filedata = await PraseCat(catFile);
+  var AllUnits : jsonDict<any> = await GetUnits(Filedata);
+  var JsdonUnits : jsonDict<any> = {}
+  for (const ID of Object.keys(AllUnits)){
+    
+    if (AllUnits[ID]["hidden"] == 'false'){
+      //console.log(AllUnits[ID]["name"]);
+      var unitData = await GetUnitData(AllUnits[ID]["name"],Filedata);
+      //console.log(unitData);
+      if (unitData != null){
+        if (unitData.hasOwnProperty("nodeName")){
+          var unitcost : jsonDict<any> = await GetUnitcosts(unitData);
+          var unitmod : jsonDict<any> = await GetUnitMods(unitData);
+          var unitCats : jsonDict<any> = await GetUnitcat(unitData);
+          //console.log(unitcost);
+          if (unitcost != null){
+            if (unitmod != null){
+            JsdonUnits[AllUnits[ID]["name"]] = {"ID":AllUnits[ID]["id"], "Cost": unitcost[IDict["Cost"]]["value"], "Size":unitmod[0]["value"], "Type": "None"};
+            } else{
+              JsdonUnits[AllUnits[ID]["name"]] = {"ID":AllUnits[ID]["id"], "Cost": unitcost[IDict["Cost"]]["value"], "Size":'1',"Type":"None"};
+            }
+            if (unitCats != null){
+              var toptype = false;
+              for (const Catkey of Object.keys(unitCats)){
+                if (!toptype){
+                  if ((unitCats[Catkey]["name"] == "Vehicle") || (unitCats[Catkey]["name"] == "Battleline") || (unitCats[Catkey]["name"] == "Infantry") || (unitCats[Catkey]["name"] == "Mounted") || (unitCats[Catkey]["name"] == "Fortification") || (unitCats[Catkey]["name"] == "Monster") || (unitCats[Catkey]["name"] == "Swarm") || (unitCats[Catkey]["name"] == "Character") || (unitCats[Catkey]["name"] == "Epic Hero") || (unitCats[Catkey]["name"] == "Dedicated Transport")){
+                    JsdonUnits[AllUnits[ID]["name"]]["Type"] = unitCats[Catkey]["name"]
+                    if ((unitCats[Catkey]["name"] == "Battleline") || (unitCats[Catkey]["name"] == "Epic Hero")){
+                      toptype = true;
+                    }
+                  }
+                }
+              }
+              
+            }
+          }
+          //console.log(JsdonUnits);
+          
+        }
+      }
+    } 
+  } 
+  return JsdonUnits;
+  //await GetUnitcosts(data);
+}
+export async function GetunitStats(unit : string, data : any){
+  var unitData = await GetUnitData(unit,data);
+  if (unitData != null){
+    var unitProf =  await GetUnitProf(unitData);
+    var unitsectionGroups =  await GetUnitsectionGroups(unitData);
+    var unitentriers=  await GetUnitsectionEntries(unitData);
+    var unitjson :  jsonDict<any> = {"Stats": null,"Invul":null,"Abilities":[], "Units":null,"Weapons":null,"Default":null};
+    if (unitProf != null){
+      for (const profkey of Object.keys(unitProf)){
+        if (unitProf[profkey]["typeName"]=="Unit"){
+          unitjson["Stats"] = {
+                              "e703-ecb6-5ce7-aec1":unitProf[profkey][IDict["Movement"]],
+                              "d29d-cf75-fc2d-34a4":unitProf[profkey][IDict["Toughness"]],
+                              "450-a17e-9d5e-29da":unitProf[profkey][IDict["Save"]],
+                              "750a-a2ec-90d3-21fe":unitProf[profkey][IDict["Wounds"]],
+                              "58d2-b879-49c7-43bc":unitProf[profkey][IDict["Leadership"]],
+                              "bef7-942a-1a23-59f8":unitProf[profkey][IDict["OC"]]
+                              };
+        }
+        if (unitProf[profkey]["name"] == "Invulnerable Save"){
+          unitjson["Invul"] = unitProf[profkey];
+        } else if (unitProf[profkey]["typeName"] == "Abilities") {
+          unitjson["Abilities"].push(unitProf[profkey]);
+        }
+      }
+    }
+    if (unitsectionGroups != null){
+      unitjson["Weapons"] = {}
+      if (Object.keys(unitsectionGroups).length == 1){
+        unitsectionGroups = unitsectionGroups[Object.keys(unitsectionGroups)[0]]
+      }
+      for (const sectionkey of Object.keys(unitsectionGroups)){
+        if (typeof unitsectionGroups[sectionkey] === 'string'){
+          if (sectionkey == "defaultSelectionEntryId"){
+            unitjson["Default"] = unitsectionGroups[sectionkey];
+          }
+
+        } else if (Object.keys(unitsectionGroups[sectionkey]).length){
+          unitjson["Weapons"][sectionkey] = unitsectionGroups[sectionkey];
+        }
+      }
+      console.log(unitsectionGroups)
+
+    }
+    if (unitentriers != null){
+      unitjson["Units"] = unitentriers;
+    }
+  console.log(unitjson);
+  return unitjson;
+  }
+  return null;
+
+}
+
+export async function SelectUnitsfromType(type : string, data : jsonDict<any>){
+  var unitsOfType : jsonDict<any> = {}
+  for (const ID of Object.keys(data)){
+    if (data[ID]["Type"] == type){
+      unitsOfType[ID] = data[ID];
+    }
+  }
+  return unitsOfType
+}
+
 
 export async function Startup(){
   await GEtW40Index();
   await ReadW40Index();
   await DownloadAllIndex("w40k_10e.json");
-  GetUnits("ImperiumAdeptusMechanicus.cat");
-  //var Data = await PraseCat("ImperiumAdeptusMechanicus.cat");
-  //console.log("Loading done")
+  var unitcosts = await GetAllUnitCosts("ImperiumAdeptusMechanicus.cat");
+  var battleunits = await SelectUnitsfromType("Battleline",unitcosts)
+  console.log(battleunits);
+  var Filedata = await PraseCat("ImperiumAdeptusMechanicus.cat");
+  var unitData = await GetUnitData("Skitarii Vanguard",Filedata);
+  await GetunitStats("Skitarii Vanguard",Filedata);
+  console.log("Done")
+  /* var unitData = await GetUnitData("Serberys Sulphurhounds","ImperiumAdeptusMechanicus.cat");
+  await GetUnitMods(unitData);
+ var unitProf =  await GetUnitProf(unitData);
+ console.log("profile")
+ console.log(unitProf)
+ unitProf =  await GetUnitcat(unitData);
+ console.log("cat")
+ console.log(unitProf)
+ unitProf =  await GetUnitMods(unitData);
+ console.log("Modifier")
+ console.log(unitProf)
+ unitProf =  await GetUnitsectionEntries(unitData);
+ console.log("Section Entries")
+ console.log(unitProf)
+ unitProf =  await GetUnitsectionGroups(unitData);
+ console.log("Section Geroups")
+ console.log(unitProf)
+ unitProf =  await GetUnitcosts(unitData);
+ console.log("UNit Cost")
+ console.log(unitProf)
+ unitProf =  await GetUnitLinks(unitData);
+ console.log("UNit Links")
+ console.log(unitProf)
+ var Data = await PraseCat("ImperiumAdeptusMechanicus.cat");
+ console.log("Loading done")*/
 }
 
 /*
